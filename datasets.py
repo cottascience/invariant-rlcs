597c33f16e18511d2fbf39c34ac0448ac52c435b
cottascience <leoabreucotta@gmail.com>
2023-05-04 22:01:54 -0400

    bla

--- datasets.py
+++ datasets.py
@@ -5,7 +5,7 @@ class Parity(Dataset):
     # Constructor
     def __init__(self, n, d):
         self.x = torch.randint(low=0, high=2, size=(n,d), dtype=torch.float32)
-        self.y = torch.pow(-1,torch.sum(x, dim=1)).unsqueeze(1)
+        self.y = torch.pow(-1,torch.sum(self.x, dim=1)).unsqueeze(1)
         self.len = n
     # Getting the data
     def __getitem__(self, index):
@@ -18,7 +18,7 @@ class Ball(Dataset):
      # Constructor
      def __init__(self, n, d, R=0.5, L=1):
          self.x = (torch.rand(n, d) - 0.5) * L
-         self.y = torch.pow(torch.tensor((torch.norm(x,dim=1) < R), dtype=float))
+         self.y = torch.pow(torch.tensor((torch.norm(self.x,dim=1) < R), dtype=float))
          self.len = n
      # Getting the data
      def __getitem__(self, index):
