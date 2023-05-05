96b45af2616019acb7ea9abd236b02c766ce4d30
cottascience <leoabreucotta@gmail.com>
2023-05-04 21:18:20 -0400

    bla

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ args = parser.parse_args()
 model = models.MLP( num_layers=args.num_layers, layer_size = args.hidden_size, input_size=args.input_size, output_size=1, dropout_p=args.dropout, use_batchnorm=True )
 
 # Define the loss function (hinge loss)
-criterion = nn.HingeEmbeddingLoss()
+criterion = nn.HingeEmbeddingLoss(margin=10)
 
 # Define the optimizer (Stochastic Gradient Descent) with L2 regularization
 optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
