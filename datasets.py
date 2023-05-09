08696a542e9ac3fd9ebfd07cf216666fe3508695
cottascience <leoabreucotta@gmail.com>
2023-05-08 20:29:56 -0400

    bla

--- datasets.py
+++ datasets.py
@@ -1,6 +1,7 @@
 import torch, math
 from torch.utils.data import Dataset
-
+from torch_geometric.utils.random import erdos_renyi_graph
+from torch_geometric.utils import to_torch_sparse_tensor
 
 class Parity(Dataset):
     # Constructor
@@ -15,6 +16,26 @@ class Parity(Dataset):
     def __len__(self):
         return self.len
 
+class Connectivity(Dataset):
+    # Constructor
+    def __init__(self, n, d):
+        p = .5
+        graphs = []
+        for i in range(n):
+            edge_index = erdos_renyi_graph(d,p)
+            A = to_torch_sparse_tensor(edge_index).to_dense()
+            print(A)
+            exit()
+
+        self.x = torch.randint(low=0, high=2, size=(n,d), dtype=torch.float32)
+        self.y = torch.pow(-1,torch.sum(self.x, dim=1)).unsqueeze(1)
+        self.len = n
+    # Getting the data
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    # Getting length of the data
+    def __len__(self):
+        return self.len
 
 class Sort(Dataset):
      # Constructor
