5aa05e03711555b53bb761e2665f51a24e203dbf
cottascience <leoabreucotta@gmail.com>
2023-05-10 16:13:55 -0400

    bla

--- models.py
+++ models.py
@@ -28,6 +28,7 @@ class RLC(torch.nn.Module):
         b = out[:,-1].unsqueeze(1)
         #print(a,b,x)
         #print(self.c1, self.c2)
+        print(a, b, self.c1, self.c2)
         return F.tanh(dot(x,self.c1*a) - self.c2*b)
 
 
