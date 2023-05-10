174a9c43280e40e2fbc8e22569d392143b940d95
cottascience <leoabreucotta@gmail.com>
2023-05-10 15:07:31 -0400

    bla

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ if torch.cuda.is_available(): model = model.cuda()
 criterion = nn.SoftMarginLoss()
 
 # Define the optimizer (Stochastic Gradient Descent) with L2 regularization
-optimizer = optim.Adagrad(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = args.epochs)
 
 # Create the data loaders
