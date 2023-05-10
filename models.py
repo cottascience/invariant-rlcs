58ec4f339f5afab6c6202f563d56fa12be2a81a2
cottascience <leoabreucotta@gmail.com>
2023-05-10 01:13:31 -0400

    bla

--- models.py
+++ models.py
@@ -22,9 +22,8 @@ class RLC(torch.nn.Module):
          self.c2 = torch.nn.Parameter(torch.ones(1))
          self.layer_norm = nn.LayerNorm(x_size+1)
      def forward(self, x):
-        #noise = self.normal.rsample([x.shape[0], self.noise_size]).to(x.device)
-        #noise = noise*self.sigma + self.mu
-        noise = torch.round(torch.rand( ( x.shape[0], self.noise_size  )))
+        noise = self.normal.rsample([x.shape[0], self.noise_size]).to(x.device)
+        noise = noise*self.sigma + self.mu
         out = self.mlp( noise )
         out = self.layer_norm(out)
         a = out[:,:-1]
