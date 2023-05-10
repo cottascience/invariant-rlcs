f31ffbca4432253adc06948fc47561ae679c8083
cottascience <leoabreucotta@gmail.com>
2023-05-09 23:28:33 -0400

    bla

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ def accuracy( model, loader ):
 
 # Train the MLP model
 for epoch in range(args.epochs):
+    epoch_loss = 0
     for x,y in train_loader:
         if torch.cuda.is_available(): x,y = x.cuda(), y.cuda()
         x = x.repeat(args.k, 1)
@@ -82,4 +83,5 @@ for epoch in range(args.epochs):
         loss = criterion(y_hat, y)
         loss.backward()
         optimizer.step()
-    print(epoch, '==\t Loss:\t', loss.item(), 'Train acc:\t', accuracy(model, train_loader).item(), 'Test acc:\t', accuracy(model, test_loader).item())
+        epoch_loss += loss.item()
+    print(epoch, '==\t Loss:\t', loss.item()/args.train_size, 'Train acc:\t', accuracy(model, train_loader).item(), 'Test acc:\t', accuracy(model, test_loader).item())
