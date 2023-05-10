d3dae41b6776133e661ab9222c5c1873cf506403
cottascience <leoabreucotta@gmail.com>
2023-05-10 15:19:22 -0400

    bla

--- models.py
+++ models.py
@@ -24,7 +24,7 @@ class RLC(torch.nn.Module):
         out = self.mlp( noise )
         a = out[:,:-1]
         b = out[:,-1].unsqueeze(1)
-        print(a,b)
+        print(a,b,x)
         return F.tanh(dot(x,self.c1*a) - self.c2*b)
 
 
