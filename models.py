b7618fd074de3641efaea5b241ae2b07243d0eba
cottascience <leoabreucotta@gmail.com>
2023-05-09 23:09:11 -0400

    bla

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class RLC(torch.nn.Module):
      def forward(self, x):
         #noise = torch.rand(( x.shape[0], self.noise_size )).to(x.device) - .5
         normal = torch.distributions.Normal(0, 1)
-        noise = normal.rsample([x.shape[0], self.noise_size])
+        noise = normal.rsample([x.shape[0], self.noise_size]).to(x.device)
         out = self.mlp( noise )
         a = out[:,:-1]
         b = out[:,-1].unsqueeze(1)
