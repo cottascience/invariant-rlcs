83de56327f5b28561e9be410bc27e3dd8f535f7c
cottascience <leoabreucotta@gmail.com>
2023-05-10 16:24:23 -0400

    bla

--- models.py
+++ models.py
@@ -32,11 +32,11 @@ class RLC(torch.nn.Module):
         #print(a, b, self.c1, self.c2)
         coin = torch.sign(torch.rand((x.shape[0],1)) - .5)
         guess = torch.sign(torch.rand((x.shape[0],1)) - .5)
-        res = F.tanh(dot(x,self.c1*a) - self.c2*b)
+        res = dot(x,self.c1*a) - self.c2*b
         for i in range(x.shape[0]):
             if coin[i] > 0:
                 res[i] = guess[i]
-        return res
+        return F.tanh(res)
 
 class RSphereC(torch.nn.Module):
       def __init__(self, noise_size, hidden_size, num_layers, dropout_p, use_batchnorm, x_size):
