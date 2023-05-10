1568d002e2b886c1125615beddb388df71bf23e1
cottascience <leoabreucotta@gmail.com>
2023-05-10 15:25:46 -0400

    bla

--- models.py
+++ models.py
@@ -16,9 +16,9 @@ class RLC(torch.nn.Module):
          self.mlp = torch_geometric_MLP(in_channels = noise_size, hidden_channels = hidden_size, out_channels = x_size+1,
                         num_layers=num_layers, norm=norm, dropout=dropout_p, act=act)
          self.noise_size = noise_size
-         self.noise_dist = torch.distributions.Normal(0,.1)
-         self.c1 = torch.nn.Parameter(torch.ones(1)*100)
-         self.c2 = torch.nn.Parameter(torch.ones(1)*100)
+         self.noise_dist = torch.distributions.Normal(10,.1)
+         self.c1 = torch.nn.Parameter(torch.ones(1)*10)
+         self.c2 = torch.nn.Parameter(torch.ones(1)*10)
          #self.layer_norm =  nn.LayerNorm(x_size+1)
      def forward(self, x):
         noise = self.noise_dist.rsample([x.shape[0], self.noise_size]).to(x.device)
