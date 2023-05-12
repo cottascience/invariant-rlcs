73b7f39e79b5aad93708ffcb261ba80671686e0b
cottascience <leoabreucotta@gmail.com>
2023-05-11 20:12:24 -0400

    bla

--- deepsets.sh
+++ deepsets.sh
@@ -5,6 +5,6 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
 #SBATCH --gres=gpu:1
-#SBATCH --array=11-20-201
+#SBATCH --array=11-201:20
 echo $SLURM_ARRAY_TASK_ID
 python main.py --dataset sort --model deepsets --lr 0.001 --input_size $SLURM_ARRAY_TASK_ID --hidden_size 25 --k 1 --m 1 --train_size 1000
