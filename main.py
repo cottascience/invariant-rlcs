7a14da4c46f77c7bba1901ea0fe453745eb95f8d
cottascience <leoabreucotta@gmail.com>
2023-05-11 23:28:11 -0400

    bla

--- main.py
+++ main.py
@@ -108,11 +108,12 @@ for run in range(args.runs):
             epoch_size += x.shape[0]
         scheduler.step()
         val_acc = accuracy(model, val_loader).item()
+        patience -= 1
         if val_acc > best_val:
             best_val = val_acc
             best_train = accuracy(model, train_loader).item()
             best_test = accuracy(model, test_loader).item()
-            patience -= 1
+            patience -= 10
         if patience == 0: break
         print(epoch, '==\t Loss:\t', epoch_loss/epoch_size, 'LR:\t',scheduler.get_lr(),'Train acc:\t', accuracy(model, train_loader).item(), 'Val acc:\t', val_acc, 'Test acc:\t', accuracy(model, test_loader).item(), 'Patience:\t', patience, best_val)
 
