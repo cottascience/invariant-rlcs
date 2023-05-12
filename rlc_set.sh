651e7ae5514cb6d8c5bfcfc39cb782cec96af1ae
cottascience <leoabreucotta@gmail.com>
2023-05-12 00:38:32 -0400

    bla

--- rlc_set.sh
+++ rlc_set.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -o deepsets_worker_%A_%a.out
+#SBATCH -o rlc_set_worker_%A_%a.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
 #SBATCH --gres=gpu:1
-#SBATCH --array=11-201:20
+#SBATCH --array=5-101:2
 echo $SLURM_ARRAY_TASK_ID
-python main.py --dataset sort --model deepsets --lr 0.001 --input_size $SLURM_ARRAY_TASK_ID --hidden_size 25 --k 1 --m 1 --train_size 1000  > sort/deepsets-$SLURM_ARRAY_TASK_ID.txt
+python main.py --dataset sort --model rlc_set --input_size $SLURM_ARRAY_TASK_ID --hidden_size 5 --train_size 1000  > sort/rlc_set-$SLURM_ARRAY_TASK_ID.txt
