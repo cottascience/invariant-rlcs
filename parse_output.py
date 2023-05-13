2e05a55437c43fffc63f7b3a5293d93e84823912
cottascience <leoabreucotta@gmail.com>
2023-05-13 17:13:46 -0400

    bla

--- parse_output.py
+++ parse_output.py
@@ -20,13 +20,15 @@ for file in files:
     start_index = file.index('-') + 1
     end_index = file.index('.')
     d.append( int( file[ start_index:end_index  ]  ))
+    c = None
     for line in lines:
         if 'Constant classifier: ' in line:
-            constant.append( float(line[ len( 'Constant classifier: ' ) :  ]  ))
+            c = float(line[ len( 'Constant classifier: ' ) :  ]  )
         if 'Final test results: ' in line:
             line = line.split(' ')
             mean.append( float(line[3]  ))
             std.append( float(line[4]  ))
+    constant.append(c)
 print( d  )
 print( mean  )
 print( std  )
