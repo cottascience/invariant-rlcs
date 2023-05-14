3d2f7f7dfb9fd805575f8f695c5820dd0f649008
cottascience <leoabreucotta@gmail.com>
2023-05-14 18:46:59 -0400

    bla

--- datasets.py
+++ datasets.py
@@ -92,6 +92,7 @@ class Ball(Dataset):
          self.x = normal.rsample([n,d])
          self.x /= self.x.norm(dim=1, keepdim=True)
          scaling_factor = 0.1
+         print('a')
          eps = torch.randn(n) * scaling_factor
          norms = torch.norm(self.x, dim=1)
          new_norms = norms + eps
