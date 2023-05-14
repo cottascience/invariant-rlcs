3f720c7dac692e2dd91bf51eec833452704cf5f5
cottascience <leoabreucotta@gmail.com>
2023-05-14 02:30:00 -0400

    bla

--- models.py
+++ models.py
@@ -47,8 +47,8 @@ class RLC(torch.nn.Module):
         ub = self.noise_dist.rsample([x.shape[0], self.noise_size]).to(x.device)
         #a = self.a(torch.cat([noise,ua],dim=1))
         #b = self.b(torch.cat([noise,ub],dim=1))
-        ab = self.ab(noise)
-        a = self.layer_norm(ab[:,:-1])
+        ab = self.layer_norm(self.ab(noise))
+        a = ab[:,:-1]
         b = ab[:,-1].unsqueeze(1)
         res = dot(x,self.c1*a) - self.c2*b
         return torch.tanh(res)
