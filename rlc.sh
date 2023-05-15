d981074e7091b6e93d9961d6e98b03ea4d7558cb
cottascience <leoabreucotta@gmail.com>
2023-05-15 01:51:25 -0400

    bla

--- rlc.sh
+++ rlc.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32GB
 #SBATCH --gres=gpu:1
-#SBATCH --array=8-32:2
+#SBATCH --array=2-10:1
 echo $SLURM_ARRAY_TASK_ID
 python main.py --dataset sin --patience 30 --model rlc --input_size $SLURM_ARRAY_TASK_ID --hidden_size $SLURM_ARRAY_TASK_ID --k 10000 > ball/rlc-$SLURM_ARRAY_TASK_ID.txt
 
