856975beabc4db372cd904bc43e159e7b4755b16
cottascience <leoabreucotta@gmail.com>
2023-05-15 01:23:30 -0400

    bla

--- datasets.py
+++ datasets.py
@@ -121,7 +121,7 @@ class Sin(Dataset):
          self.f = torch.sin( math.pi * math.pow(d//2+1,3) * dot( self.x, self.x_ ) )
          self.x = torch.cat([self.x, self.x_], dim=1)
          self.y = 2*(torch.tensor((self.f < 0), dtype=float)).unsqueeze(1) - 1
-         print(x.shape, y.shape)
+         print(self.x.shape, self.y.shape)
          self.len = n
          print('Ratio  +/-:\t', torch.sum((self.y + 1)/2)/n )
          if log: print('Constant classifier:', max( [ 1 - (torch.sum((self.y + 1)/2)/n).item(), (torch.sum((self.y + 1)/2)/n).item()  ]  ) )
