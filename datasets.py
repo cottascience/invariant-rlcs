3f78cb178a18437302495ed7169ce8ebc2b921aa
Leonardo De Abreu Cotta <leonardodeabreucotta@gilda.local>
2023-08-03 15:32:47 -0400

    bla

--- datasets.py
+++ datasets.py
@@ -91,7 +91,7 @@ class Range(Dataset):
           normal = torch.distributions.Normal(100, 10)
           self.x = normal.rsample( [n,d ] )
           f = torch.max(self.x, dim=1)[0] - torch.min(self.x, dim=1)[0]
-          self.y = 2*(torch.tensor(( f < 50), dtype=float)).unsqueeze(1) - 1
+          self.y = 2*(torch.tensor(( f < 25), dtype=float)).unsqueeze(1) - 1
           self.len = n
           self.f = f
           print('Ratio  +/-:\t', torch.sum((self.y + 1)/2)/n )
