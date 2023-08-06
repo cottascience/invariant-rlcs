cee84fdcc66c07333846b5886f2763ce3323a174
Leonardo De Abreu Cotta <leonardodeabreucotta@gilda.ht.home>
2023-08-06 14:26:36 -0400

    bla

---
+++ deepsets_easy_large.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH -o deepsets_worker_%A_%a.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:1
+#SBATCH --array=5-101:4
+echo $SLURM_ARRAY_TASK_ID
+python main.py --dataset range --model deepsets --input_size $SLURM_ARRAY_TASK_ID --hidden_size 5 --k 1 --m 1 --train_size 1000 --patience 50 --lr 0.01 --num_layers 2  > range/deepsets-$SLURM_ARRAY_TASK_ID.txt
